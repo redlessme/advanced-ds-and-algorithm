10cd264d17dee60d375c2cfa042a7755480b47db
redlessme <41262162+redlessme@users.noreply.github.com>
2020-07-12 19:31:43 +1000

    Add files via upload
    encoder-decoder

--- A3/lzss_decoder.py
+++ A3/lzss_decoder.py
@@ -1,82 +1,65 @@
-# Name: Siyuan Yan
-# ID: 28498704
 import sys
-from bitarray import bitarray
-def decode(header):
-    """start to decode header"""
-    res=''
-    i=0
-    #decode number of unique number
-    encoder_dict={}
-    decoder_dict={}
-    move,unique_num= elias_val(header,i)
-    i += move #i=3
-    # print("m",move)
-    for _ in range(unique_num):
-        ascii_code=header[i:i+8]
-        char=chr(int(ascii_code,2)) #a
-
-        i+=8
-        i,next_read=elias_val(header,i)
-        huff_code=header[i:i+next_read]
-        i+=next_read
-        encoder_dict[char]=huff_code
-        decoder_dict[huff_code]=char
-
-    """ start decode data part"""
-    i,total_field_num=elias_val(header,i)
-    for _ in range(total_field_num):
-        if header[i]=='1':
-            i+=1
-            find_huff=header[i]
-            count=1
-
-            while find_huff not in decoder_dict:
-                count+=1
-                find_huff=header[i:i+count]
-            res+=decoder_dict[find_huff]
-            i+=len(find_huff)
-        elif header[i]=='0':
-            i+=1
-            i,offset=elias_val(header,i)
-            i,mathed_length=elias_val(header,i)
-            read_pos=len(res)-offset
-            pt1=read_pos
-            for _ in range(mathed_length):
-                res+=res[pt1]
-                pt1+=1
-    return res
 
 
-"""decode the elias codes to original value"""
-def elias_val(bin_num,cur):
-    L=1
-    while True:
-        if bin_num[cur]== '0':
-            bin_num= bin_num[:cur] + '1' + bin_num[cur + 1:]
-            bi_num= bin_num[cur:cur + L]
-            L=int(bi_num,2)
-            cur+=len(bi_num)
-            L+=1
-        if bin_num[cur]=='1':
-            res=bin_num[cur:cur+L]
-            cur+=L
-            break
-    next_read=int(res,2)
-    return cur,next_read
-
-if __name__ == '__main__':
+def decoder(bits):
+    k = 1
+    z = 0
+    value = bits[z:z + k]
+    while value[0] == '0':
+        value = '1' + value[1::]
+        _next = z+k
+        z = _next
+        k = int(value, 2) + 1
+        _next = z+k
+        value = bits[z: _next]
+    return int(value,2), _next
 
 
-    text = sys.argv[1]
-    txt = bitarray()
-    f1 = open(text, "rb")
-    txt.fromfile(f1)
-    f1.close()
-    txt = txt.to01()
+inputFile = sys.argv[1]
 
-    result = decode(txt)
-    f2=open("output_lzss_decoder.txt",'w')
-    f2.write(result)
-    f2.close()
+with open(inputFile, 'r') as myfile:
+    bits=myfile.read().replace('\n', '')
 
+
+W = sys.argv[2]
+L = sys.argv[3]
+
+W = int(W)
+L = int(L)
+
+#bits = '10110000110110000110110001100110001001011000100011011101100001'
+
+
+
+curr = 0
+final = []
+
+while curr < len(bits):
+    if bits[curr]=='0':
+        offset, next1 = decoder(bits[curr+1::])
+        length, next2 = decoder(bits[curr+1+next1::])
+        temp = (0,offset,length)
+        final += [temp]
+        curr+=next1+next2+1
+    elif bits[curr]=='1':
+        char = chr(int(bits[curr+1:curr+9], 2))
+        temp = (1,char)
+        final += [temp]
+        curr+=9
+
+
+string = ''
+for i in range(len(final)):
+    tup = final[i]
+    if tup[0] == 1:
+        string+=tup[1]
+    elif tup[0] == 0:
+        offset = tup[1]
+        length = tup[2]
+        double = string[len(string)-offset::]*2
+        string += double[0:length]
+
+#print(string)
+file = open('output_lzss_decoder.txt', 'w')
+file.write(string)
+file.close()
--- A3/lzss_encoder.py
+++ A3/lzss_encoder.py
@@ -1,136 +1,328 @@
-# Name: Siyuan Yan
-# ID: 28498704
-from huffman_code import *
-from Boyer_Moore import *
 import sys
-from bitarray import bitarray
+# lzss
 
-"""convert string into 8-bits ascii"""
-def to_ascii(_string):
+def preprocess(data):
     """
-    :param _string: a character
-    :return: a 8 bits ascii
+    Reference: Monash Tutor
     """
-    res=''
-    for char in _string:
-        bits=bin(ord(char))[2:]
-        bits='0'*(8-len(bits))+bits
-        res+=bits
-    return res
-"""generate the number's elias code like L3L2L1N"""
-def elias(num):
-    """
-    :param num
-    :return: elias code
-    """
-    N=bin(num)[2:]
-    L=len(N)
-    L-=1
 
-    while L>1:
-        bin_L=bin(L)[2:]
-        bin_L='0'+bin_L[1:]
-        N = bin_L + N
-        L=len(bin_L)-1
-    if L==1:
-        N='0'+N
-    return N
-"""construct header"""
-def generate_header(text):
-    if len(text)==0:
-        return ""
-    header = ''
-    uni_list = []
-    for char in text:
-        if char not in uni_list:
-            uni_list.append(char)
-    uni_char_num = len(uni_list)
-    uni_char_num = elias(uni_char_num)
-    header += uni_char_num
-    ##sort
-    uni_list = sorted(uni_list)
+    arr = [0] * len(data)
+    arr[0] = len(data) #put the length of the string in the first position
+    left = 0 #alignment
+    right = 0 #alignment
 
-    for char in uni_list:
-        ascii_char = to_ascii(char)
-        header += ascii_char
-        huff_code = encode_dict[char]
-        len_huff = len(huff_code)
-        len_huff = elias(len_huff)
-        header += len_huff
-        header += huff_code
-    return header
-"""lzss to generate the data part, I also use the BoyerMoore Algorithm to find the matched substring  """
-def lzss(text,w,l):
-    # """construct data part"""
-    if len(text) == 0:
-        return ""
-    i = 0
-    res = []
-    data=''
-    while i < len(text):
-        buf = text[i:i + l]
-        if i - w < 0:
-            dict = text[0:i]
+    #start from pos 1 till the end of data
+    for i in range(1, len(data)):
+        #if outside the z-box, then we need to count
+        if i > right:
+            count = 0
+            # while we are still within the string
+            # and the letter is the same as the prefix
+            # count keeps track of the prefix
+            while i + count < len(data) and data[count] == data[i+count]:
+                # increment count
+                count += 1
+            # set the z-value at i
+                arr[i] = count
+            # if there is a box, update the box boundary
+            if count > 0:
+                left = i
+                right = i + count - 1
+        # if inside the box
         else:
-            dict = text[i - 6:i]
-        window = dict + buf
-        if buf[0] not in dict:
-            format = (1, buf[0])
-            res.append(format)
-            i += 1
-            continue
-        else:
-            matched = ''
-            length = 0
-            offset = 0
-            for j in range(len(buf)):
-                matched += buf[j]
-                index_list = BM(window, matched)
-                # print("indedx_list",index_list)
-                index_list1 = []
-                for index in index_list:
-                    if index < len(dict):
-                        index_list1.append(index)
-                if len(index_list1) == 0:
-                    break
-                length += 1
-                offset = len(dict) - max(index_list1)
-            if length < 3:
-                format = (1, buf[0])
-                i += 1
+            # get the paired prefix index (z-value) to copy
+            index_prefix = i - left
+            # the remaining length of the box
+            remaining = right - i + 1
+            #case 2a:
+            #if the z-value at prefix is still within the remaining box
+            if arr[index_prefix] < remaining:
+                # copy the value
+                arr[i] = arr[index_prefix]
+            # case 2b
+            # if the value is the exactly the box
+            # then we need to extend/ find a new box
+            elif arr[index_prefix] == remaining:
+                new_right = right + 1
+                while new_right < len(data) and data[new_right] == data[new_right-i]:
+                    new_right += 1
+                # update the z-array with the extension
+                    arr[i] = new_right - i
+                # new left and right boundary
+                left = i
+                right = new_right - 1
+            # case 2c
+            # if the value is greater than the remaining (going outside the box)
+            # then we know only the remaining of the box is the same as the prefix
+            # note this is z_array[index_prefix] > remaining
             else:
-                format = (0, offset, length)
-                i += length
-            res.append(format)
-    total_number=elias(len(res))
-    data+=total_number
-    for tuple in res:
-        data+=str(tuple[0])
-        if tuple[0]==1:
-            data+=encode_dict[tuple[1]]
-        if tuple[0]==0:
-            data+=elias(tuple[1])
-            data+=elias((tuple[2]))
-    return data
+                arr[i] = remaining
+    return arr
 
-if __name__ == '__main__':
 
-    name=sys.argv[1]
-    w=sys.argv[2]
-    l=sys.argv[3]
-    w=int(w)
-    l=int(l)
-    f=open(name,'r')
-    text=f.read()
-    dict = calculate_Frequency(text)
-    tree = buildHuffmanTree(dict)
-    if len(dict)>1:
-        label(tree,'')
-    encode_dict,decode_dict=get_dict()
-    head=generate_header(text)
-    data = lzss(text,w,l)
-    result=head+data
-    result=bitarray(result)
-    f=open("output_lzss_encoder.bin","wb")
-    result.tofile(f)
-    f.close()
+
+def badCharPreprocess(pat):
+    #preprocess the pattern and store the last occurrence of every possible character in an array of size = alphabet size
+
+    #create a 2D table which is indexed first by the index of the character c in the alphabet and second by the index i in the pattern
+    #This lookup will return the occurrence of c in P with the next-highest index j<i or -1 if there is no such occurrence.
+    #array[c] return the left occurrence character.
+    #The proposed shift will then be {\displaystyle i-j} i-j, with O(1) lookup time and O(kn) space (a finite alphabet of length k).
+
+    if len(pat) == 0:
+        return [[] for a in range(26)]
+    table = [[-1] for a in range(26)]
+    alpha = [-1 for a in range(26)]
+    for i in range(len(pat)):
+        char = ord(pat[i].lower())-ord('a')
+        alpha[char] = i #converts the pattern character and puts it in alpha
+        for j in range(len(alpha)):
+            table[j].append(alpha[j])
+    return table
+
+
+#Reference, Book by Dan Gusfield: Algorithms on Strings, Trees and Sequences (1997)
+def goodSuffix1(S):
+    L = [-1] * len(S)
+    N = preprocess(S[::-1])  # S[::-1] reverses S
+    N.reverse()
+    for j in range(len(S) - 1):
+        # Using 𝑁_𝑗, we can define 𝐿′(𝑖) as
+        # the largest j so that  𝑁_𝑗 (𝑃) = |𝑡| = 𝑛−𝑖+1.
+        i = len(S) - N[j]
+        if i != len(S):
+            L[i] = j
+    return L
+
+
+def goodSuffix2(S):
+    #initialize an array 0 times the length of the pattern
+    l = [0]*len(S)
+    #pre-process the the pattern
+    Z = preprocess(S)
+    Z = list(reversed(Z))
+    largest = 0
+    for i in range(len(Z)):
+        #if the values in the Z array = i+1, pick the largest of the value or largest value already stored
+        if Z[i] == i+1:
+            largest = max(Z[i], largest)
+        #start putting the largest element from the back fro the l
+        l[len(l)-1-i] = largest
+    return l
+
+
+""""
+Reference: http://www-di.inf.puc-rio.br/~laber/StringMatching.pdf
+Reference2: https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_string-search_algorithm#The_Galil_Rule
+"""
+def boyerMoore(pat, txt):
+    #Galils Rule: shift sections that are known to match
+    matches = []
+
+    #Preprocessing
+    bd = badCharPreprocess(pat)
+    L = goodSuffix1(pat)
+    l = goodSuffix2(pat)
+
+
+    current = len(pat) - 1      # alignment of last value of Pattern corresponding to Text
+    previous = -1     #alignment of previous phase
+    while current < len(txt):
+        matched = False
+        i = len(pat) - 1  #Patttern
+        t = current     #Text
+        #start from the end character of pattern
+        # while the pattern and text matches, decrease index of pattern and text
+        while i >= 0 and t > previous and pat[i] == txt[t]:   # Matches starting from end of P with its corresponding character in T
+            i -= 1
+            t -= 1
+        if i == -1 or t == previous:  #Match
+            index = current - len(pat) #the index of the matched item
+            matches.append(index + 1) #append match index into the list
+            matched = True
+            if len(pat)>1:
+                current += len(pat) - l[1] #update the current counter by the case
+            else:
+                current +=1 #increment the current counter
+        #else:   # No match, shift by max of bad character and good suffix rules
+        if matched != True:
+            # for good suffix below
+            if i + 1 == len(pat):  # Mismatch happened on first attempt
+                goodSuffix = 1
+            elif L[i + 1] == -1:  # Matched suffix does not appear anywhere in P
+                #the least amount for a prefix of P to match a suffix of t –
+                # i.e. using a non-zero value of 𝑙’(𝑖).
+                goodSuffix = len(pat) - l[i + 1]
+            else:  # Matched suffix appears in P
+                #find a copy of 𝑡 which is preceded by a different character –
+                # i.e. using a non-zero value of 𝐿’(𝑖).
+                goodSuffix = len(pat) - L[i + 1]
+
+            #for bad character below
+            char = ord(txt[t].lower()) - ord('a') #convert the text value which has mismatched
+            badChar = i - bd[char][i] #subtract the pattern position(i) with the pre-processed value in the specific position
+
+            #choosing between bad character and good suffix
+            if badChar >= goodSuffix:
+                shift = badChar
+            else:
+                shift = goodSuffix
+
+           #Galils rule
+            if shift>= i+1:
+                previous=current
+            current += shift
+    return matches
+
+
+
+
+"""
+To convert from a string to bits
+"""
+def tobits(string):
+    final = ''
+    for character in string:
+        bits = bin(ord(character))[2:]
+        bits = '00000000'[len(bits):] + bits
+        final+=bits
+    return final
+
+"""
+Elias variable-length prefix-free code for offset and length
+For integers
+Format-0 requirement
+"""
+def elias(num):
+    arr = []
+    var = ""
+    # convert to bitstring
+    value = list(bin(num)[2:])
+    if value[0] == 1: #if the leading bit is a 1,
+        value[0] = 0 #change to 0
+    arr.append(var.join(value))
+    L = len(value)  # length
+    while L>1:
+        var = ""
+        L -=1
+        value = list(bin(L)[2:])
+        if value[0] == bin(1)[2:]:
+            value[0] = bin(0)[2:]
+        arr.append(var.join(value))
+        L = len(value) #update length
+    var = ""
+    for i in reversed(arr):
+        var+=i
+    return var
+
+
+
+
+
+
+
+inputFile = sys.argv[1]
+
+with open(inputFile, 'r') as myfile:
+    text=myfile.read().replace('\n', '')
+
+W = sys.argv[2]
+L = sys.argv[3]
+
+W = int(W)
+L = int(L)
+
+#text = 'aacaacabcaba'
+# W = 6
+# L = 4
+
+result = []
+window = []  # dictionary
+preview = []  # length of the longest match
+
+
+
+i = 0
+while i < (len(text)):
+    nextChar = text[i]
+    # if the character in already present
+    if ''.join(preview + [nextChar]) in ''.join(window + preview):
+        if len(preview) < L:
+            preview.append(nextChar)
+            i += 1
+
+        if len(preview) == L:
+            dictionary = ''.join(window + preview)
+            offset = len(window) - boyerMoore(preview,dictionary)[0]
+            nextChar = text[i]
+            result.append((0, offset, len(preview)))
+            result.append((1, nextChar))
+            window += preview
+            window.append(nextChar)
+            if len(window) > W:  # if dictionary size gets bigger than 6, chop of the first few values to make dict size 6
+                var = len(window) - W
+                window = window[var:]
+            i += 1
+            preview = []
+
+
+    else:  # if the character is not present
+        if len(preview) == 0:
+            result.append((1, nextChar))
+            window.append(nextChar)
+            i += 1
+        else:
+            dictionary = ''.join(window + preview)
+            if len(preview) < 3:
+                for j in preview:
+                    result.append((1, j))
+                window += preview
+                preview = []
+            else:
+                offset = len(window) - boyerMoore(preview, dictionary)[0]
+                nextChar = text[i]
+                result.append((0, offset, len(preview)))
+                result.append((1, nextChar))
+                window += preview
+                window.append(nextChar)
+                if len(window) > W:  # if dictionary size gets bigger than 6, chop of the first few values to make dict size 6
+                    var = len(window) - W
+                    window = window[var:]
+
+                i += 1
+                preview = []
+
+if len(preview) > 0:
+    if len(window) > W:  # if dictionary size gets bigger than 6, chop of the first few values to make dict size 6
+        var = len(window) - W
+        window = window[var:]
+    nextChar = preview.pop()
+    result.append((1, nextChar))
+
+#print(result)
+
+arr = ''
+for i in result:
+    string = ''
+    if i[0]==1:
+          string+=str(1)
+          string+=tobits(i[1])
+          arr+=string
+    else:
+        string+=str(0)
+        string+=elias(i[1])
+        string+=elias(i[2])
+        arr+=string
+
+file = open('output_lzss_encoder.txt', 'w')
+file.write(arr)
+file.close()
+# print(arr)
+# check = '10110000110110000110110001100110001001011000100011011101100001'
+# print(check)
+
+# if arr == check:
+#     print("working")
+# else:
+#     print("not working")
+
